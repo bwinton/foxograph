57f1512f9c93a47675da1597580b3b8574cee863
Blake Winton <bwinton@latte.ca>
2012-09-19 13:25:33 -0400

    Log all requests and response codes.

--- app.js
+++ app.js
@@ -2,16 +2,52 @@
  * Module dependencies.
  */
 
+var ejs = require('ejs');
 var express = require('express');
 var routes = require('./routes');
+var url = require('url');
 
 var app = module.exports = express();
+
+const logTmpl = ejs.compile('<%= date %> (<%= response_time %>ms): ' +
+                            '<%= status %> <%= method %> <%= url %>');
 
 // Configuration
 
 app.configure(function(){
   app.set('views', __dirname + '/www');
-  app.engine('html', require('ejs').renderFile);
+  app.engine('html', ejs.renderFile);
+  app.use(function(req, res, next) {
+    var rEnd = res.end;
+
+    // To track response time
+    req._rlStartTime = new Date();
+
+    // Setup the key-value object of data to log and include some basic info
+    req.kvLog = {
+      date: req._rlStartTime.toISOString(),
+      method: req.method,
+      url: url.parse(req.originalUrl).pathname,
+    };
+
+    // Proxy the real end function
+    res.end = function(chunk, encoding) {
+      // Do the work expected
+      res.end = rEnd;
+      res.end(chunk, encoding);
+
+      // And do the work we want now (logging!)
+
+      // Save a few more variables that we can only get at the end
+      req.kvLog.status = res.statusCode;
+      req.kvLog.response_time = (new Date() - req._rlStartTime);
+
+      // Send the log off to winston
+      console.log(logTmpl(req.kvLog));
+    };
+
+    next();
+  });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
