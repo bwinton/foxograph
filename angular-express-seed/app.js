724145e89f101955c6336e673a3b1ad18da50f85
Blake Winton <bwinton@latte.ca>
2013-05-31 12:49:15 -0700

    Start using Angular-Express-Seed instead.

---
+++ angular-express-seed/app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  routes = require('./routes'),
+  api = require('./routes/api');
+
+var app = module.exports = express();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+// Start server
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
--- public/app/js/services.js
+++
@@ -1,6 +0,0 @@
-define(['angular'], function (angular) {
-  'use strict';
-  
-  angular.module('myApp.services', [])
-    .value('version', '0.1');
-});
