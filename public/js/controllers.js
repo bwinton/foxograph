31654049972079f3d35d4c9c2c7e6b89617b75dc
Blake Winton <bwinton@latte.ca>
2013-06-14 14:14:24 -0400

    Okay, I'm liking this controller setup _way_ more. Also, remove unused css, and add bugzillaMockup.js.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -58,35 +58,52 @@ function loadImage(imageSrc, callback)
 
 foxographApp.controller({
 
+  // The ProjectsCtrl handles getting the list of projects, selecting a
+  // project, and automatically selecting the appropriate mockup in that
+  // project.
   'ProjectsCtrl': function ProjectsCtrl($scope, $location, $route, $routeParams, $resource) {
-    var routeChange = function routeChange() {
-      $scope.project = _.findWhere($scope.projects, {_id: $routeParams.p_id});
-      if (!$scope.selectedProject) {
-        $scope.selectedProject = $scope.project;
-      }
-    };
 
+    // Handle the change in route by setting the various ids.
+    var routeChange = function routeChange() {
+      console.log("$routeParams (project) = " + JSON.stringify($routeParams));
+      $scope.p_id = $routeParams.p_id;
+      $scope.m_id = $routeParams.m_id;
+    };
     $scope.$on("$routeChangeSuccess", routeChange);
 
-    $scope.setBackground = function (background) {
-      $scope.background = background;
-    };
 
-    $scope.changeProject = function () {
-      var project = $scope.selectedProject;
-      $location.path('/' + (project ? project._id : ''));
+    // Handle a change in project id by setting the project.
+    var changeProject = function changeProject() {
+      if (!$scope.projects) {
+        // console.log("No projects yet.  Skipping " + $scope.p_id + "…");
+        return;
+      }
+      $scope.project = _.findWhere($scope.projects, {_id: $scope.p_id});
+      // console.log("$scope.project = " + $scope.project);
     };
+    $scope.$watch('p_id', changeProject);
 
+
+    // Handle a change in mockup id by setting the mockup.
+    var changeMockup = function changeMockup() {
+      if (!$scope.mockups) {
+        //console.log("No mockups yet.  Skipping " + $scope.m_id + "…");
+        return;
+      }
+      $scope.mockup = _.findWhere($scope.mockups, {_id: $scope.m_id});
+      // console.log("$scope.mockup = " + $scope.mockup);
+    };
+    $scope.$watch('m_id', changeMockup);
+
+
+    // Load in the projects.
     projects($resource).query(function (projectList) {
       $scope.projects = projectList;
-      routeChange();
+      changeProject();
     });
-  },
 
-  'MockupCtrl': function MockupCtrl($scope, $resource) {
-    // Handle changes to the currently selected project.
+
     $scope.$watch('project', function (project) {
-      console.log("Got project of " + project);
       if (!project) {
         $scope.mockups = null;
         $scope.mockup = null;
@@ -94,45 +111,85 @@ foxographApp.controller({
         //$scope.setBackground('');
         return;
       }
-      projectBugs($resource).query({p_id: project._id}, function (bugList) {
-        $scope.bugs = bugList;
-      });
       mockups($resource).query({p_id: project._id}, function (mockupList) {
         $scope.mockups = mockupList;
-        if (mockupList.length > 0) {
-          $scope.mockup = {};
-          $scope.mockup._id = mockupList[0]._id;
-          $scope.mockup.name = mockupList[0].name;
-          $scope.mockup.image = 'background-image: url("/r/images/bugzilla-loading.png");';
-          $scope.mockup.width = 'width: 100%;';
-          $scope.mockup.height = 'height: 100%;';
-          $scope.mockup.position = 'background-position: 45%;';
-          var image = mockupList[0].image;
-          if (!image) {
-            image = '/r/images/default.png';
-          }
-          var ctx = document.getElementById('background-canvas').getContext('2d');
-          loadImage(image, function MockupView_loadImage(img) {
-            ctx.drawImage(img, 1 - img.width, 1 - img.height);
-            var imgData = ctx.getImageData(0, 0, 1, 1);
-            var pixel = 'background-color: rgb(' + imgData.data[0] + ',' + imgData.data[1] + ',' + imgData.data[2] + ');';
-            $scope.$apply(function () {
-              $scope.mockup.image = 'background-image: url("' + image + '");';
-              if (mockupList[0].image) {
-                $scope.mockup.width = 'width: ' + img.width + 'px;';
-                $scope.mockup.height = 'height: ' + img.height + 'px;';
-                $scope.mockup.position = '';
-              }
-              $scope.setBackground(pixel);
-            });
-          });
-        }
+        changeMockup();
       });
+    });
+
+    // Event handlers!
+    $scope.setBackground = function setBackground(background) {
+      $scope.background = background;
+    };
+
+    $scope.onProjectSelect = function onProjectSelect() {
+      var project = $scope.selectedProject;
+      $location.path('/' + (project ? project._id : ''));
+    };
+
+
+  },
+
+  'MockupCtrl': function MockupCtrl($scope, $route, $routeParams, $resource) {
+    // Handle changes to the currently selected project.
+    $scope.$watch('project', function (project) {
+      if (!project) {
+        return;
+      }
+      projectBugs($resource).query({p_id: project._id}, function (bugList) {
+        $scope.bugs = bugList;
+        setTimeout(function () {
+          $scope.$apply(function () {
+            //console.log("Running!  1");
+            run();
+          });
+        }, 15);
+      });
+    });
+    $scope.$watch('mockups', function (mockupList) {
+      if (mockupList && mockupList.length > 0) {
+        console.log("(Sub) Got mockups!!!");
+        // $scope.mockup = {};
+        // $scope.mockup._id = mockupList[0]._id;
+        // $scope.mockup.name = mockupList[0].name;
+        // $scope.mockup.image = 'background-image: url("/r/images/bugzilla-loading.png");';
+        // $scope.mockup.width = 'width: 100%;';
+        // $scope.mockup.height = 'height: 100%;';
+        // $scope.mockup.position = 'background-position: 45%;';
+        // var image = mockupList[0].image;
+        // if (!image) {
+        //   image = '/r/images/default.png';
+        // }
+        // var ctx = document.getElementById('background-canvas').getContext('2d');
+        // loadImage(image, function MockupView_loadImage(img) {
+        //   ctx.drawImage(img, 1 - img.width, 1 - img.height);
+        //   var imgData = ctx.getImageData(0, 0, 1, 1);
+        //   var pixel = 'background-color: rgb(' + imgData.data[0] + ',' + imgData.data[1] + ',' + imgData.data[2] + ');';
+        //   $scope.$apply(function () {
+        //     $scope.mockup.image = 'background-image: url("' + image + '");';
+        //     if (mockupList[0].image) {
+        //       $scope.mockup.width = 'width: ' + img.width + 'px;';
+        //       $scope.mockup.height = 'height: ' + img.height + 'px;';
+        //       $scope.mockup.position = '';
+        //     }
+        //     $scope.setBackground(pixel);
+        //   });
+        // });
+      }
     });
 
     // Handle changes to the currently selected mockup.
     $scope.$watch('mockup', function (mockup) {
-      console.log("Got mockup of " + mockup);
+      //console.log("Got mockup of " + mockup);
+      if (!mockup) {
+        return;
+      }
+      setTimeout(function () {
+        $scope.$apply(function () {
+          //console.log("Running!  2");
+          run();
+        });
+      }, 15);
       bugs($resource).query({m_id: mockup._id}, function (bugList) {
         $scope.mockup.bugs = bugList;
       });
---
+++ public/js/lib/bugzillaMockup.js
@@ -0,0 +1,213 @@
+//bugzillaMockup.js by Alex Faaborg, based on bugzilla.js by Atul Varma
+//details on how to leverage this file here: http://people.mozilla.com/~faaborg/files/firefox4Mockups/liveMockups.png
+
+
+// bugzilla.js by Atul
+var Bugzilla = {
+  BASE_URL: "https://api-dev.bugzilla.mozilla.org/latest",
+  BASE_UI_URL: "https://bugzilla.mozilla.org",
+  DEFAULT_OPTIONS: {
+    method: "GET"
+  },
+  getShowBugURL: function Bugzilla_getShowBugURL(id) {
+    return this.BASE_UI_URL + "/show_bug.cgi?id=" + id;
+  },
+  queryString: function Bugzilla_queryString(data) {
+    var parts = [];
+    for (name in data) {
+      var values = data[name];
+      if (!values.forEach)
+        values = [values];
+      values.forEach(
+        function(value) {
+          parts.push(encodeURIComponent(name) + "=" + encodeURIComponent(value));
+        });
+    }
+    return parts.join("&");
+  },
+  ajax: function Bugzilla_ajax(options) {
+    var newOptions = {__proto__: this.DEFAULT_OPTIONS};
+    for (name in options)
+      newOptions[name] = options[name];
+    options = newOptions;
+
+    function onLoad() {
+      var response = JSON.parse(xhr.responseText);
+      if (!response.error) {
+        options.success(response);
+      } else {
+        if (options.hasOwnProperty("failure") && typeof(options.failure) == "function")
+          options.failure(response);
+      }
+    }
+
+    var xhr = options.xhr ? options.xhr : new XMLHttpRequest();
+    var url = this.BASE_URL + options.url;
+
+    if (options.data)
+      url = url + "?" + this.queryString(options.data);
+    xhr.open(options.method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("load", onLoad, false);
+    xhr.send(null);
+    return xhr;
+  },
+  getBug: function Bugzilla_getBug(id, cb, eb) {
+    return this.ajax({url: "/bug/" + id,
+                      success: cb,
+                      failure: eb});
+  },
+  search: function Bugzilla_search(query, cb) {
+    return this.ajax({url: "/bug",
+                      data: query,
+                      success: cb});
+  }
+};
+window.bugzilla = Bugzilla;
+
+//add an onload function to the existing mockup file
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+      window.onload = func;
+    }
+    else {
+      window.onload = function() {
+      if (oldonload) {
+          oldonload();
+      }
+        func();
+      };
+    }
+}
+
+
+function run(){
+  debug("Loaded bugzilaMockup.js");
+
+  window.scroll(0,0);
+
+  //Find all of the new divs added in Fireworks and overlay new bug panels
+  var newPanels = [];
+  var bugNumber;
+  var div;
+  var divs =  document.getElementsByTagName("div");
+  for(var i = 0; i < divs.length; i++){
+    div = divs[i];
+
+    if(div.id.substr(0,4) == "bug-"){
+      //debug("found a bug div: " + div.id);
+      if (div.classList.contains("panel")) continue;
+
+      newPanels.push(div);
+
+      div.classList.add("panel");
+    }
+  }
+
+  //Set all of the panels to the loading state so they fade in
+  for(var j = 0; j < newPanels.length; j++){
+    div = newPanels[j];
+    div.classList.add("loading");
+    div.innerHTML = "<img src='/images/bugzilla-loading.png'>";
+
+    //Populate each panel with live data from bugzilla
+    bugNumber = div.id.substr(4,6);
+    var bug = Bugzilla.getBug(bugNumber,fillData);
+  }
+
+  //set the scroll position if the URL had a hash
+  location.hash = location.hash;
+
+}
+
+//Inject an onload event to add the bug data
+addLoadEvent(run);
+
+
+//Populate a specific panel with bug data
+function fillData(bug){
+
+  var div = document.getElementById("bug-" + bug.id);
+  var panelClass;
+  var blockingClass = "inactive";
+  var assignedClass = "inactive";
+
+  var status = "At Risk";
+  if(bug.status == "RESOLVED" || bug.status == "VERIFIED"){
+    if(bug.resolution == "FIXED"){
+      status = "Fixed";
+    }
+    if(bug.resolution == "DUPLICATE"){
+      status = "Duplicate";
+    }
+    if(bug.resolution == "INVALID"){
+      status = "Invalid";
+    }
+    if(bug.resolution == "WONTFIX"){
+      status = "Won't Fix";
+    }
+    if(bug.resolution == "WORKSFORME"){
+      status = "Works for Me";
+    }
+    if(bug.resolution == "INCOMPLETE"){
+      status = "Incomplete";
+    }
+  }
+
+  var assigned = "Unassigned";
+  if(bug.assigned_to.real_name != "Nobody; OK to take it and work on it"){
+    assigned = bug.assigned_to.real_name;
+    assignedClass = "active";
+  }
+
+  var blocking = "Not blocking";
+  if(bug.cf_blocking_20 !== null){
+    if(bug.cf_blocking_20.substr(-1) === "+"){blocking = "Blocking"; blockingClass = "active";}
+    if(bug.cf_blocking_20 == "-"){blocking = "Blocking rejected"; blockingClass = "rejected";}
+    if(bug.cf_blocking_20 == "?"){blocking = "Nominated"; blockingClass = "inactive";}
+  }
+
+  if(status == "Fixed"){
+    panelClass = "complete";
+  }
+  else if(status == "Duplicate"){
+    panelClass = "duplicate";
+  }
+  else if(status == "Invalid" || status == "Won't Fix" || status == "Works for Me" || status == "Incomplete"){
+    panelClass = "atRisk";
+  }
+  else if(blocking == "Nominated"){
+    status = "Nominated";
+    panelClass = "nominated";
+  }
+  else if(blocking == "Blocking" ){
+    status = "Blocker";
+    panelClass = "blocking";
+  }
+  else if(assigned != "Unassigned"){
+    status = "Grassroots";
+    panelClass = "grassRoots";
+  }
+  else if((blocking == "Not blocking" || blocking == "Blocking rejected") && assigned == "Unassigned"){
+    status = "At Risk";
+    panelClass = "atRisk";
+  }
+
+  div.className = "panel done " + panelClass;
+
+  var inner = "<a href='https://bugzilla.mozilla.org/show_bug.cgi?id=" + bug.id + "'>" + bug.id + ", " + bug.summary + "</a><br>" +
+  "<span class=" + panelClass + ">" + status + ":</span> " +
+  "<span class=" + blockingClass + ">" + blocking + "</span>, " +
+  "<span class=" + assignedClass + ">" + assigned + "</span>";
+
+  if (div.getAttribute('owned') === 'true')
+    inner += '<a class="link deleteBug"><i class="icon-trash icon-white"></i></a>';
+  div.innerHTML = inner;
+}
+
+
+function debug(message){
+  console.log(message);
+}
