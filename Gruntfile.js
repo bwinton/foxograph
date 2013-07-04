1ac195dd378c0c10b832495f9fcc431562dce54b
Blake Winton <bwinton@latte.ca>
2013-07-04 13:55:53 -0400

    Add a js-hint tool to grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,44 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    jshint: {
+      files: ['public/js/**/*.js'],
+      options: {
+        ignores: [
+          'public/js/lib/angular/*.js',
+          'public/js/lib/bugzillaMockup.js',
+          'public/js/lib/jquery.js',
+          'public/js/lib/underscore.js'
+        ],
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          require: true,
+          define: true,
+          requirejs: true,
+          describe: true,
+          expect: true,
+          it: true,
+        }
+      },
     }
   });
 
   grunt.registerTask('version', 'Write the version.', function() {
     grunt.log.write(grunt.config('pkg.name') + ' ' + grunt.config('pkg.version') + '\n').ok();
   });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
   grunt.registerTask('default', ['version']);
