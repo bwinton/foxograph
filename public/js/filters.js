1001a74798c3eecf8a45d6ee18807d4676a09a1d
Blake Winton <bwinton@latte.ca>
2013-11-11 15:39:49 -0500

    Remove unused filter and icons.

--- public/js/filters.js
+++ public/js/filters.js
@@ -12,32 +12,7 @@
 
 /* Filters */
 
-foxographApp.filter('bugLineStyle', function styleFactory() {
-  return function bugLineStyle(bug) {
-    if (!bug) {
-      return '';
-    }
-
-    // Move the ending line down a little so that it ends up near the middle of the div.
-    var endX = bug.endX + 25;
-    var endY = bug.endY + 50;
-
-    // Set up some intermediate variables to calculate with.
-    var width = endX - bug.startX;
-    var height = endY - bug.startY;
-    var length = Math.sqrt(width * width + height * height);
-    var angle = Math.atan2(height, width);
-
-    // And finally make the css rules.
-    var top = 'top:' + bug.startY + 'px; ';
-    var left = 'left:' + bug.startX + 'px; ';
-    var transform = 'transform:rotate(' + angle + 'rad) scaleX(' + length + '); ';
-
-    // top:{{bug.startY}}px; left:{{bug.startX}}px; transform: rotate({{angle}}rad) scaleX({{length}});
-    var out = top + left + transform;
-    return out;
-  };
-}).filter('statusClass', function styleFactory() {
+foxographApp.filter('statusClass', function styleFactory() {
   return function statusClass(bug) {
     if (!bug) {
       return '';
